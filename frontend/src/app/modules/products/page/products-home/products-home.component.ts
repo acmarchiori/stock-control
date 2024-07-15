293881cfea4ac3e43064fd94f4f92c538ee97854
acmarchiori <ac_marchiori@yahoo.com.br>
2024-07-15 19:50:43 -0300

    <Feat><Implementado rota e tela de produtos, busca de dados de produtos, Dumb Component da tabela de produtos, funcionalidade de deletar produto, service de categories e método de buscar categorias >

---
+++ frontend/src/app/modules/products/page/products-home/products-home.component.ts
@@ -0,0 +1,125 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
+
+@Component({
+  selector: 'app-products-home',
+  templateUrl: './products-home.component.html',
+  styleUrls: []
+})
+export class ProductsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public productsDatas: Array<GetAllProductsResponse> = [];
+
+  constructor(
+    private productsService: ProductsService,
+    private productsDtService: ProductsDataTransferService,
+    private router: Router,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getServiceProductDatas();
+  }
+
+  getServiceProductDatas() {
+    const productsLoaded = this.productsDtService.getProductsDatas();
+
+    if(productsLoaded.length > 0) {
+      this.productsDatas = productsLoaded;
+    } else this.getAPIProductsDatas();
+
+    console.log('DADOS DE PRODUTOS', this.productsDatas);
+
+  }
+
+  getAPIProductsDatas() {
+    this.productsService
+    .getAllProducts()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        if(response.length > 0) {
+          this.productsDatas = response;
+          console.log('DADOS DE PRODUTOS', this.productsDatas);
+
+        }
+      },
+      error: (err) => {
+        console.log(err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao buscar produtos',
+          life: 2500,
+        })
+        this.router.navigate(['/dashboard']);
+      }
+    })
+  }
+
+  handleProductAction(event: EventAction): void {
+    if(event) {
+      console.log('DADOS DO EVENTO RECEBIDO', event);
+    }
+  }
+
+  handleDeleteProductAction(event: {
+    product_id: string,
+    productName: string
+  }): void {
+    if(event) {
+      this.confirmationService.confirm({
+        message: `Confirma a exclusão do produto: ${event?.productName}?`,
+        header: 'Confirmação de exclusão',
+        icon: 'pi pi-exclamation-triangle',
+        acceptLabel: 'Sim',
+        rejectLabel: 'Não',
+        accept: () => this.deleteProduct(event?.product_id),
+      })
+    }
+  }
+
+  deleteProduct(product_id: string) {
+    if(product_id) {
+      this.productsService
+        .deleteProduct(product_id)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            if(response) {
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Produto removido com sucesso!',
+                life: 2500,
+              });
+
+              this.getAPIProductsDatas();
+            }
+          }, error: (err) => {
+            console.log(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao remover produto!',
+              life: 2500,
+            })
+
+          }
+        });
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+}
